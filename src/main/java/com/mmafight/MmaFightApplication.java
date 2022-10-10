ee2171f5502a7a7c53ef1ec62be4c7702839f6c1
Maycon Jardim <mayconsmill@gmail.com>
2022-10-09 21:42:48 -0300

    Projeto criado

---
+++ src/main/java/com/mmafight/MmaFightApplication.java
@@ -0,0 +1,13 @@
+package com.mmafight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmaFightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmaFightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmafight/MmaFightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmafight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmaFightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
